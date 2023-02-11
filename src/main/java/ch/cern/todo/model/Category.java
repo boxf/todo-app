8b03c5ea2bd74af2564f7b88d4b64ac7e183bbcc
boxf <fboxebeld@gmail.com>
2023-02-11 19:50:58 +0100

    Importing Lombok

--- src/main/java/ch/cern/todo/model/Category.java
+++ src/main/java/ch/cern/todo/model/Category.java
@@ -1,5 +1,14 @@
 package ch.cern.todo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+/**
+ * Entity representing category's task in the app
+ */
+@Data
+@AllArgsConstructor
 public class Category {
 
     private Long id;
--- src/main/java/ch/cern/todo/model/Task.java
+++ src/main/java/ch/cern/todo/model/Task.java
@@ -1,7 +1,15 @@
 package ch.cern.todo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.sql.Timestamp;
 
+/**
+ * Entity representing the task in the app
+ */
+@Data
+@AllArgsConstructor
 public class Task {
 
     private Long id;
